93d0fba6daae5927f7f4cf45e56c073128ae0133
Sofia <kugaevasofia1302@gmail.com>
2025-08-10 11:46:49 +0300

    work with was separated

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import os, csv
+import os
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
 from dotenv import load_dotenv
 
 from models import db, Exercises, Users
+
+from _csv import to_csv
 
 load_dotenv()
 
@@ -173,25 +175,8 @@ def add_workout():
         db.session.add(new_exercise)
         db.session.commit()
 
-        # * работа с csv
-        exercises = {
-            "user_id": user.id,
-            "exercise": f"{exercise}",
-            "amount": amount,
-        }  # * словарь для записи упражнений для дальнейшего переноса в csv файл
-
-        filename = "D:/PythonProjects/workouts_site/data/data.csv"
-
-        # * проверяем, нужно ли писать заголовки (если файл пустой/не существует)
-        write_header = not os.path.exists(filename) or os.stat(filename).st_size == 0
-
-        with open(filename, "a", newline="") as csvfile:
-            fieldnames = exercises.keys()
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-            if write_header:
-                writer.writeheader()  # * записываем загловки только если файл пустой
-            writer.writerow(exercises)  # * записываем одну строку данных
+        # * запись в csv
+        to_csv(user_id=user.id, exercise=exercise, amount=amount)
 
         return redirect(url_for("index"))
 
